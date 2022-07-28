4c1bf4890de8e3360959712b51e6391f71ede12a
Alexinder Virdee <alex.virdee08@gmail.com>
2022-07-28 14:45:22 -0400

    Initial content display area

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,23 @@ const Home = () => {
       <Hero>
         <Heading>Welcome to F1 Racing Live Viewer</Heading>
       </Hero>
+      <Main>
+        <RaceContent>
+          <div>Item 1</div>
+          <div>Item 2</div>
+          <div>Item 3</div>
+        </RaceContent>
+        <RaceContent>
+          <div>Item 1</div>
+          <div>Item 2</div>
+          <div>Item 3</div>
+        </RaceContent>
+        <RaceContent>
+          <div>Item 1</div>
+          <div>Item 2</div>
+          <div>Item 3</div>
+        </RaceContent>
+      </Main>
     </>
   )
 }
@@ -19,8 +36,20 @@ const Hero = styled.div`
   border: 4px solid blue;
 `
 
-const Heading = styled.h1`
-  padding-bottom: 2px;
+const Heading = styled.h3`
+  padding-bottom: 8px;
+`
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 4px dashed purple;
+`
+
+const RaceContent = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
 `
 
 export default Home
